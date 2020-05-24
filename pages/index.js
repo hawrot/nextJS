9eb4c87bb19314d55d26ca8a1bd440af046b0b05
hawrot <mhawro200@caledonian.ac.uk>
2020-05-24 23:36:20 +0100

    Change the index.js into class component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>The main page</h1>
-        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth') }>Go to Auth</button>
-    </div>
-);
+class IndexPage extends Component {
+    render() {
+        return (
 
+            <div>
+                <h1>The main page</h1>
+                <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+                <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+            </div>
 
-export default indexPage;
+        )
+    };
+}
+
+export default IndexPage;
