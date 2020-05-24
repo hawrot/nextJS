00c92e263865773b8a733de556fcba6167d09190
hawrot <mhawro200@caledonian.ac.uk>
2020-05-24 23:32:17 +0100

    Add specific 404 page

---
+++ pages/_error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+
+const errorPage = () => (
+    <div>
+        <h1>Wrong Page</h1>
+
+    </div>
+);
+
+
+export default errorPage;
