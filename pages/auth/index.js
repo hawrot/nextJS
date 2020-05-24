b8dd22c6389009c5a380e636377a5313e094a3d4
hawrot <mhawro200@caledonian.ac.uk>
2020-05-24 23:30:21 +0100

    Working with styles

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -4,7 +4,15 @@ import User from '../../components/User';
 const authIndexpage = () => (
     <div>
         <h1>The auth index page</h1>
-        <User name="Max" age={25} />
+        <User name="Max" age={25}/>
+        <style jsx>
+            {`
+            div {
+                border: 1px solid #eee;
+                text-align: center;
+            }
+            `}
+        </style>
     </div>
 );
 
