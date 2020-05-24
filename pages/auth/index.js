a037797f7c69ade2249a01e2370c63057ba9d1e0
hawrot <mhawro200@caledonian.ac.uk>
2020-05-24 23:25:30 +0100

    Working with components and props

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexpage = () => (
     <div>
         <h1>The auth index page</h1>
+        <User name="Max" age={25} />
     </div>
 );
 
